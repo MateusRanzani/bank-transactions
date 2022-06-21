4833c37230b95e06410c0ff0fb5ed883b5445d95
MateusRanzani <mateusranzani@hotmail.com>
2022-06-21 11:46:41 -0300

    Created deposit route

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const {cpf} = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({error: "Customer not found"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.get("/account", (request, response) => {
     return response.json(customers);
 })
@@ -31,16 +45,26 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 })
 
-app.get("/statement", (request, response) => {
-    const {cpf} = request.headers;
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const {customer} = request;
+    return response.json(customer.statement)
+})
 
-    const customer = customers.find(customer => customer.cpf === cpf);
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
 
-    if(!customer) {
-        return response.status(400).json({error: "Customer not found"});
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
     }
 
-    return response.json(customer.statement)
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 })
 
 app.listen(3333);
